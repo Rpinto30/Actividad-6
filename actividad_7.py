b5da9f247b56264adc488552fbfb3f3a32363993
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-24 21:11:33 -0600

    Agrego la opción 1 y comienzo a agregar sus respectivas opciones

--- actividad_7.py
+++ actividad_7.py
@@ -2,7 +2,7 @@
 def entry_nums():
     while True:
         n = input("\n>  Ingrese una cantidad de números: ")
-        if not n.isdigit(): print("\nLo siento, intentelo de nuevo")
+        if not n.isdigit(): print("\n>  Lo siento, intentelo de nuevo\n")
         else: break
     nums = []
     for i in range(int(n)):
@@ -10,7 +10,7 @@ def entry_nums():
             try:
                 num = int(input(f"{i + 1}) Ingresa numero: "))
                 break
-            except ValueError : print("\nLo siento, intentelo de nuevo")
+            except ValueError : print("\n>  Lo siento, intentelo de nuevo\n")
         nums.append(num)
     return nums
 
@@ -21,12 +21,22 @@ def total_sum(list_num):
 
 def average(list_num): return total_sum(list_num)/len(list_num)
 
-def mayor_that(list_num, mayor, txt):
+def mayor_that(list_num, mayor, txt, minus):
     pos, neg = 0,0
     for i in list_num:
         if i>=mayor: pos+=1
         else: neg +=1
-    print(txt)
+    if minus: print(f"Hay {neg} {txt} menores a {mayor}")
+    else:print(f"Hay {pos} {txt} mayores a {mayor}")
+
+def pos_neg(list_num):
+    zeros, pos, neg = 0,0,0
+    for i in list_num:
+        if i == 0: zeros+=1
+        elif i>=0: pos+=1
+        else: neg +=1
+    print(f"\n>  La cantidad de positivos es {pos} y de negativos es {neg}\n")
+    print(f">  La cantidad de ceros es: {zeros}")
 
 #FUNCIONES EXCLUSIVAS DEL EJERCICIO 7
 def mult_3(list_num):
@@ -40,14 +50,14 @@ def rect_area(base, height): return base*height
 def rect_perimet(x,y): return 2*(x+y)
 
 def prime_number(num):
-    if num <= 1 or int(num) != num: print("\nSolo pueden ser considerados primero los enteros positivos mayores que 1")
+    if num <= 1 or int(num) != num: print("\n>  Solo pueden ser considerados primero los enteros positivos mayores que 1\n")
     else:
         for i in range(2,num):
             print(i)
             if num%i == 0:
-                print(f"\nEl número {num} no es primo")
+                print(f"\n>  El número {num} no es primo\n")
                 break
-        else: print(f"\nEl número {num} es primo")
+        else: print(f"\n>  El número {num} es primo\n")
 
 def mayor_minus(num_list,txt):
     mayor = num_list[0]
@@ -65,7 +75,7 @@ def repeat_on_list(num_list):
         else:
             if i not in repeat: repeat[i] = 2 #detecta si ya existia uno anterior igual al actual, por eso 2
             else: repeat[i] += 1
-
+    print("")
     print(f"{'Repetidos':<15}{'Frecuencia'}")
     for j in repeat: print(f"{j:<15}{repeat[j]}")
 
@@ -81,6 +91,15 @@ while True:
     print("1) Ingresar n números reales\n2) Calcular el perimetro y area de un rectángulo\n3) Verificar si un número es primo\n4) Calcular el promedio de n calificaciones\n5) Ingresar n números enteros\n6) Calculadora\n7) Salir")
     op = input("Ingrese una de las opciones: ")
     match op:
-
+        case '1':
+            nums = entry_nums()
+            while True:
+                print("-"*15+f"Que desea hacer con {nums}?"+"-"*15)
+                print("  1) Suma total\n  2) Promedio\n  3) Cantidad de positivos, negativos y ceros\n  4) Cuantos son multiplos de 3\n  5) Salir")
+                op_1 = input("Ingrese una de las opciones: ")
+                match op_1:
+                    case '1': print(f"\n>  La suma total es {total_sum(nums)}")
+                    case '5': break
+                    case _: print("\n>  Entrada no valida, intente nuevamente")
         case '7': break
-        case _: print("Entrada no valida, intente nuevamente")
+        case _: print("\n>  Entrada no valida, intente nuevamente")
